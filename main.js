6d589dd9ae1b5e5cbcfae64f9081fa8c8add5bc2
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-03-04 11:08:57 -0300

    alterando animacao e identando o codigo

--- main.js
+++ main.js
@@ -1,37 +1,37 @@
-const debounce = function(func, wait, immediate) {
-    let timeout;
-    return function(...args) {
-      const context = this;
-      const later = function () {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      const callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+const debounce = function (func, wait, immediate) {
+  let timeout;
+  return function (...args) {
+    const context = this;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
     };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
   };
-const target=document.querySelectorAll('[data-anime]');
-const animationClass= 'animate';
+};
+const target = document.querySelectorAll("[data-anime]");
+const animationClass = "animate";
 
-function AnimeScroll(){
-    const windowTop = window.pageYOffset + ((window.innerHeight *3)/4);
-    target.forEach(function(element) {
-        if((windowTop)> element.offsetTop){
-            element.classList.add(animationClass);
-        }
-        else{
-            element.classList.remove(animationClass);
-        }
-
-    })
+function AnimeScroll() {
+  const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+  target.forEach(function (element) {
+    if (windowTop > element.offsetTop) {
+      element.classList.add(animationClass);
+    } else {
+      element.classList.remove(animationClass);
+    }
+  });
 }
 AnimeScroll();
 
-
-if(target.length) {
-    window.addEventListener('scroll', debounce(function() {
+if (target.length) {
+  window.addEventListener(
+    "scroll",
+    debounce(function () {
       AnimeScroll();
-    }, 150));
-  }
+    }, 100)
+  );
+}
